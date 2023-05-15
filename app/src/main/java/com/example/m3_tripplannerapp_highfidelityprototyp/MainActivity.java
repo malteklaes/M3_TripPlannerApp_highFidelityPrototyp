13e877fa64653c9a31e597c2ff6c8fc91b23b29a
Malte <mail@malteklaes.dk>
2023-05-15 11:32:03 +0200

    Neuer Commit: erste TestDatei und toString() im DataConnection.java

---
+++ app/src/main/java/com/example/m3_tripplannerapp_highfidelityprototyp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.m3_tripplannerapp_highfidelityprototyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DataConnection dataConnection = new DataConnection("Vienna", "Hbf", "Köln", "Hbf");
+
+        Log.d("logger", "Ergebnis: " + dataConnection.toString());
+
+
+    }
+}
