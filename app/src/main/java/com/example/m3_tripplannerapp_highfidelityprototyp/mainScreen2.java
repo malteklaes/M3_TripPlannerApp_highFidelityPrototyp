2086369a002154141e87dc433e50f0e24194e910
Milenko Dukic <mildukic97@gmail.com>
2023-05-15 15:22:05 +0200

    [mainScreen2] added Activity mainScreen2

---
+++ app/src/main/java/com/example/m3_tripplannerapp_highfidelityprototyp/mainScreen2.java
@@ -0,0 +1,14 @@
+package com.example.m3_tripplannerapp_highfidelityprototyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainScreen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen2);
+    }
+}
